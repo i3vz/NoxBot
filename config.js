38bc3a57dfff76316497f14bd9c9f136f5b62b6c
3vz <51035404+i3vz@users.noreply.github.com>
2022-07-11 19:51:51 +0200

    fix

--- config.js
+++ config.js
@@ -2,12 +2,9 @@ module.exports = {
     app: {
         px: '-',
         owners: '519503123656605699',
-<<<<<<< Updated upstream
       mongodb: 'mongodb+srv://3vz:Iyassbilalsy1234@cluster0.e7qzf56.mongodb.net/?retryWrites=true&w=majority',
-=======
         serverid: '613504635956887562',
         defaultroleid: '938119043855040522',
->>>>>>> Stashed changes
     },
 
     opt: {
