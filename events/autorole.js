c514664864b3ddae9ce28811d4ddaffeb67d98e7
3vz <51035404+i3vz@users.noreply.github.com>
2022-07-11 20:30:47 +0200

    Member

--- events/autorole.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client) => {
-    client.on('guildMemberAdd', member => {
-        if(client.guilds.cache.get(config.serverid)) {
-            return member.roles.add(member.guild.roles.cache.get(config.defaultroleid));
-        }
-    });
-  };
